b9da06b61d51d7402956114f46069b905f16eeea
Chengxuan Xing <xcxncu@gmail.com>
2015-06-06 23:42:07 +0100

    Logical Expression Added

--- index.js
+++ index.js
@@ -13,45 +13,25 @@ function login(){
 		url = url+ '?$expand=Dimensions';	
 	}
 	xhrGet(url, function(res) {
-	//Cube 2<a id="col_div_cube2" href="javascript:collapse('div_cube2');">Show</a>
-	//<br/>
-	//<div id="div_cube2" style="display:none;text-align:center;">
-	//<table>
-	//<thead><th width='600px'>Rules</th>
-	//<th width='600px'>Feeders</th>
-	//<th width='600px'>Demesions</th></thead>
-	//<tbody style="text-align:center">
-//	<tr><td>rulsadfsadfsdafsdafsdafsdafsadfsadfsadfes</td>
-//	<td>sadfsadfdsafsdafsadfdsafsdafasdfasdfsadf</td>
-//	<td>sadfsdafsdafsdafasdfsadfsafsdafsadfsdaf</td>
-//	</tr>
-//	</tbody>
-//	</table>
-//	</div>
-
 		var rdiv = document.getElementById('div_result');
-		rdiv.innerHTML = "";
-		document.getElementById('div_result_null').innerHTML = "";
-		var data = res.value;
-		for(var i in data){
-			
-			var rfs = data[i].Rules.toString().trim();
-			rfs = removeComments(rfs);
-			rfs = rfs.replace(/;\n+/g,";<br/><br/>").replace(/\s+/g,"");
-			if(rfs.length>0){
-				var rs = '';
-				var fs = '';
-				var ds = '';
-				var atest = '';
+	rdiv.innerHTML = "";
+	document.getElementById('div_result_null').innerHTML = "";
+	var data = res.value;
+	for(var i in data){
+		var rfs = data[i].Rules.toString().trim();
+		rfs = removeComments(rfs);
+		rfs = rfs.replace(/;\n+/g,";<br/><br/>").replace(/\s+/g,"");
+		if(rfs.length>0){
+			var rs = '';
+			var fs = '';
+			var ds = '';
+			var atest = '';
 			var s = tm1parser.parse(rfs);
 			var jcontent = s.RULES_FILE;
 			var scontent = rfs.split(";<br/><br/>").filter(Boolean);
 			for(var sc in scontent){
 				scontent[sc] = 	scontent[sc].replace(/</g,"&lt").replace(/>/g,"&gt");		
 			}
-
-
-
 			if(jcontent.length==2){
 				var jrules = jcontent[0].RULES;
 				var jfeeders = jcontent[1].FEEDERS;
@@ -66,67 +46,82 @@ function login(){
 					for (var x in jrules) {
 							var atestfeeders = "";
 							for (var y in jrules[x]) {
-								if (y == "RULE_DEF") {
+								if (y == "RULE_DEF") {	
 									var rarea = digstrings(jrules[x][y][0]);
+									if(Array.isArray(rarea[0])){
+										rarea = rarea[0];									
+									}
 									var rexp = digstrings(jrules[x][y][1]);
+									for(var re in rexp){
+										if(Array.isArray(rexp[re][0])){
+											rexp[re] = rexp[re][0];
+										}									
+									}
 									var brexp = rexp.slice(0);
 									for(var f in jfeeders){
 		   							var farea = digstrings(jfeeders[f][0]);
 										var fexp = digstrings(jfeeders[f][1]);
-										if((pc(rarea,fexp)||pc(fexp,[rarea[0]]))&&cc(rexp,farea)){//(pc(fexp,rarea)||pc(rarea,fexp))){
+										if(farea.length>0&&fexp.length>0){
+											if(Array.isArray(farea[0])){
+												farea = farea[0];
+											}
+											if(Array.isArray(fexp[0])){
+												fexp = fexp[0];
+											}
+										for(var ex in rexp){
+										if((fullyContains(rarea,fexp)||fullyContains(fexp,[rarea[0]]))&&hasIntersection(rexp[ex],farea)){//(pc(fexp,rarea)||pc(rarea,fexp))){
 										 //atest +="<tr><td>"+srules[x] +"</td><td>"+sfeeders[f]+"</td><td></td></tr>";
 										 atestfeeders+=sfeeders[f] + ";<br/><br/>";
-										 brexp = pr(brexp,farea);
+										 brexp[ex] = excludeSame(brexp[ex],farea);
 										 feederchk.push(f);
 										}
 									}
-									if(brexp.length>0){
-									atestfeeders+= "<font color=\"green\">[" + brexp.join(",") +"]=>[" + rarea.join(",") +"];</font><br/>";
-								}
+										
+									}
+									}
+									if(!hasEmptyArray(brexp)){
+									atestfeeders+= "<font color=\"red\">[" + getLeast(brexp).join(",") +"]=>[" + rarea.join(",") +"];</font><br/>";
+									}
 									atest +="<tr><td>"+srules[x] +"</td><td>"+atestfeeders+"</td><td></td></tr>";
 								}
 							}
 							
 						}
 						
-					for(var x=0;x<sfeeders.length;x++){
-						if(feederchk.indexOf(x.toString())==-1){
+						for(var x=0;x<sfeeders.length;x++){
+							if(feederchk.indexOf(x.toString())==-1){
 								fs += sfeeders[x] + ";<br/><br/>";				
+							}
 						}
-					}
 			}
-			}else{
-				var jrules  = jcontent[0].RULES;
-				rs = scontent.join(";<br/><br/>");
-			}
-				
-				var dim = data[i].Dimensions;
-				for(var d in dim){
-				ds += dim[d].Name + '<br/>';	
-				}
-				rdiv.innerHTML += data[i].Name + "<a id=\"col_div_"+data[i].Name
-			    +"\" href=\"javascript:collapse('div_"+ data[i].Name +"');\">Show</a><br/>";
-				var cdiv = document.createElement('div');
-				cdiv.setAttribute("id", "div_"+data[i].Name);
-				cdiv.setAttribute("style", "display:none;text-align:center;");
-				var ctable = document.createElement('table');
-				ctable.setAttribute("border","1");
-				var cth = document.createElement('thead');
-				cth.innerHTML = "<th width='500px'>Rules</th><th width='500px'>Feeders</th><th width='100px'>Demesions</th>";
-				var ctb = document.createElement('tbody');
-				ctb.setAttribute("style", "text-align:center;");
-				ctb.innerHTML = atest +"<tr><td>"+rs+"</td><td style=\"color:red;\">"+fs+"</td><td>"+ds+"</td></tr>";
-				ctable.appendChild(cth);
-				ctable.appendChild(ctb);
-				cdiv.appendChild(ctable);
-				rdiv.appendChild(cdiv);
-			}else{
-				document.getElementById('div_result_null').innerHTML += data[i].Name+"<br/>";
-			}
-			
-				
+		}else{
+			var jrules  = jcontent[0].RULES;
+			rs = scontent.join(";<br/><br/>");
 		}
-	}, function(err) {
+		var dim = data[i].Dimensions;
+		for(var d in dim){
+			ds += dim[d].Name + '<br/>';	
+		}
+		rdiv.innerHTML += data[i].Name + "<a id=\"col_div_"+data[i].Name +"\" href=\"javascript:collapse('div_"+ data[i].Name +"');\">Show</a><br/>";
+		var cdiv = document.createElement('div');
+		cdiv.setAttribute("id", "div_"+data[i].Name);
+		cdiv.setAttribute("style", "display:none;text-align:center;");
+		var ctable = document.createElement('table');
+		ctable.setAttribute("border","1");
+		var cth = document.createElement('thead');
+		cth.innerHTML = "<th width='500px'>Rules</th><th width='500px'>Feeders</th><th width='100px'>Demesions</th>";
+		var ctb = document.createElement('tbody');
+		ctb.setAttribute("style", "text-align:center;");
+		ctb.innerHTML = atest +"<tr><td>"+rs+"</td><td style=\"color:red;\">"+fs+"</td><td>"+ds+"</td></tr>";
+		ctable.appendChild(cth);
+		ctable.appendChild(ctb);
+		cdiv.appendChild(ctable);
+		rdiv.appendChild(cdiv);
+	}else{
+			document.getElementById('div_result_null').innerHTML += data[i].Name+"<br/>";
+	}
+	}
+}, function(err) {
 		alert(err);
 	},document.getElementById("txt_UN").value.trim(),document.getElementById("txt_PW").value.trim());
 }
---
+++ tmexp.js
@@ -0,0 +1,231 @@
+function fullyContains(a1,a2){
+	if(a1&&a2){
+	for(var i=0;i<a2.length;i++){
+		if(a1.indexOf(a2[i])==-1){
+			return false;
+		}
+	}
+}
+	return true;
+}
+
+function hasEmptyArray(a) {
+	for(var i in a){
+		if(Array.isArray(a[i])){
+		if(a[i].length == 0 ) return true;	}
+	}
+}
+
+function getLeast(a){
+	var mini = 9999;
+	var minindex = -1;
+	for(var i in a){
+		if(Array.isArray(a[i])){
+		if(a[i].length <mini){
+			mini = a[i].length;
+			minindex = i;		
+		}	}
+	}
+	return a[minindex];
+}
+
+function hasIntersection(a1,a2){
+	if(a1&&a2){
+	for(var i=0;i<a2.length;i++){
+		if(a1.indexOf(a2[i])!=-1){
+			return true;
+		}
+	}
+}
+	return false;
+}
+
+function excludeSame(a1,a2){
+	if(a1&&a2){
+	for(var i=0;i<a2.length;i++){
+		if(a1.indexOf(a2[i])!=-1){
+			a1.splice(a1.indexOf(a2[i]),1);
+		}
+	}
+}
+	return a1;
+}
+
+function removeComments(origin){
+var tmp ="";
+if(origin.indexOf("#")!=-1){
+while(origin.indexOf("#")!=-1){
+	
+	if(origin.indexOf("#")>0){
+		tmp+=origin.substring(0,origin.indexOf("#"));
+			
+	}
+	if(origin.indexOf("\n")!=-1){
+	origin = origin.substring(origin.indexOf("\n",origin.indexOf("#")+1)+1);
+}else{
+origin = "";
+}	
+	if(origin.indexOf("#")==-1){
+		if(origin!=""){
+			tmp+=origin;	
+		}
+	
+	}
+}
+}else {
+tmp = origin;
+}
+tmp = tmp.replace(/ +/g,"");
+return tmp;
+}
+
+function removeDuplicates(a) {
+	var x = [];
+	for(var i=0;i<a.length;i++){
+	if(x.indexOf(a[i])==-1){
+		x.push(a[i]);
+	}
+}
+	return x;
+}
+
+var digstringsa = function myself(x) {
+	var tmp = [];
+	if (Array.isArray(x)) {
+		for (var y in x) {
+			tmp = tmp.concat(myself(x[y]));
+		}
+	} else {
+
+		if (x.STRING) {
+			tmp.push(x.STRING.toString().trim());
+		} else {
+			var ps = Object.keys(x);
+			for (var j = 0; j < ps.length; j++) {
+				var nm = ps[j];
+				var v = x[nm];
+				if( nm.toString()  == "AREA_ITEMS" && v.AREA_ITEM_SET){
+					tmp = tmp.concat(myself(v.AREA_ITEM_SET));
+				}
+				if (Array.isArray(v)) {
+					if(nm.toString() !="FUNCTION"){
+					tmp = tmp.concat(myself(v));
+					}else{
+						 switch(x[nm][0].IDENTIFIER.toString()) {
+						 	case "ATTRS":
+						 	break;
+						 	case "ATTRN":
+						 	break;
+						 	default:
+						 	 tmp = tmp.concat(myself(v));
+						 	 break;
+						 } 					
+					}
+				} else {
+					if (v.STRING) {
+						tmp.push(v.STRING.toString().trim());
+					}
+
+				}
+			}
+		}
+	}
+	return removeDuplicates(tmp);
+};
+
+var digstrings = function myself(x) {
+	var tmp = [];
+	if (Array.isArray(x)) {
+		for (var y in x) {
+			tmp = mergeArray(tmp,myself(x[y]));
+		}
+	} else {
+
+		if (x.STRING) {
+			tmp.push(x.STRING.toString().trim());
+		} else {
+			var ps = Object.keys(x);
+			for (var j = 0; j < ps.length; j++) {
+				var nm = ps[j];
+				var v = x[nm];
+				if( nm.toString()  == "AREA_ITEMS" && v.AREA_ITEM_SET){
+					tmp = mergeArray(tmp,myself(v.AREA_ITEM_SET));
+				}
+				if (Array.isArray(v)) {
+					var ttmp = [];
+					if(nm.toString() !="FUNCTION"){
+						switch(nm.toString()) {
+							case "*":
+							tmp.push(myself(v[0]));
+							tmp.push(myself(v[1]));
+							break;
+							case "/":
+							tmp = mergeArray(tmp,myself(v[1]));
+							break;
+							default:
+							tmp = mergeArray(tmp,myself(v));
+							break;						
+						}
+
+					}else{
+						 switch(x[nm][0].IDENTIFIER.toString()) {
+						 	case "ATTRS":
+						 	break;
+						 	case "ATTRN":
+						 	break;
+						 	case "IF":
+						 	tmp = mergeArray(tmp,myself(v[1].EXPR_LIST[1]));
+						 	tmp = mergeArray(tmp,myself(v[1].EXPR_LIST[2]));
+						 	break;
+						 	default:
+						 	 tmp = mergeArray(tmp,myself(v[1].EXPR_LIST));
+						 	 break;
+						 } 					
+					}
+				} else {
+					if (v.STRING) {
+						tmp.push(v.STRING.toString().trim());
+					}
+
+				}
+			}
+		}
+	}
+	return removeDuplicates(tmp);
+};
+
+function mergeArray(a,b){
+	if(a&&b){
+		if(a.length==0){
+return b;		
+		}
+		if(b.length ==0){
+return a;		
+		}
+var tmp = [];
+		for(var x in a){
+			if(Array.isArray(a[x])){
+				for(var y in b){
+				if(Array.isArray(b[y])){
+					tmp.push(removeDuplicates(a[x].concat(b[y])));
+				}else{
+					tmp.push(removeDuplicates(a[x].concat(b)));
+					break;
+				}
+				}
+			}else{
+				for(var y in b){
+				if(Array.isArray(b[y])){
+					tmp.push(removeDuplicates(a.concat(b[y])));
+				}	else{
+				tmp.push(removeDuplicates(a.concat(b)));
+				break;
+				}	
+			}	
+			}
+		}
+	return tmp;
+	}else{
+return [];	
+	}
+}
