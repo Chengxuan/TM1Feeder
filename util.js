477f5c9cf52fc1de6317febe52fdb5173b0dc0ee
Chengxuan <xcxncu@gmail.com>
2015-08-20 21:16:08 +0100

    streamline util.js Eliminating functions for cookies.

--- util.js
+++ util.js
@@ -53,33 +53,6 @@ function objectToQuery(map) {
     return pairs.join("&");
 }
 
-function createCookie(name, value, days) {
-    var expires;
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
-    } else {
-        expires = "";
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function getCookie(name) {
-    if (document.cookie.length > 0) {
-        start = document.cookie.indexOf(name + "=");
-        if (start != -1) {
-            start = start + name.length + 1;
-            end = document.cookie.indexOf(";", start);
-            if (end == -1) {
-                end = document.cookie.length;
-            }
-            return unescape(document.cookie.substring(start, end));
-        }
-    }
-    return null;
-}
-
 function collapse(elementId) {
     var ele = document.getElementById(elementId);
     if (ele.style.display == "block") {
